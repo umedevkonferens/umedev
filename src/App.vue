8e028307363ccaf368677dc394cfbd5cd5437de5
mattiasbpersson <mattiasbpersson@gmail.com>
2019-10-31 21:21:53 +0100

    Ta bort länk i headern

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,6 @@
         <span class="font-weight-light">DEV</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
