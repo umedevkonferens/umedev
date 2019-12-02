39cfadb9664cc33b4bfe117b962fe61e3deb690e
mattiasbpersson <mattiasbpersson@gmail.com>
2019-12-02 21:18:29 +0100

    Add support for firebase

---
+++ src/db.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+// Get a Firestore instance
+export const db = firebase
+  .initializeApp({ databaseURL: 'https://umedev-web.firebaseio.com/' })
+  .database()
