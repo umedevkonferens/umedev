bf4bf29dd349198b38618ef971bedfc481ea1311
Andrea Berglund <andreaberglund@outlook.com>
2019-12-11 12:29:40 +0100

    No more center alignments

--- src/views/Program.vue
+++ src/views/Program.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="about">
     <v-container>
-      <v-row class="mb-6" justify="center">
+      <v-row class="mb-6" justify="flex-start">
         <v-col md="auto">
           <h1 class="display-2 mb-3">Program</h1>
         </v-col>
       </v-row>
-      <v-row class="mb-6" justify="center">
+      <v-row class="mb-6" justify="flex-start">
         <v-col md="auto">Programmet, när det finns något</v-col>
       </v-row>
     </v-container>
