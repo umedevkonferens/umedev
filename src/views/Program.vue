fb99d0e6a61b0c4f3ee4c183e4623481a7acd3da
Andrea Berglund <andreaberglund@outlook.com>
2020-01-13 14:23:23 +0100

    Added last years program.

--- src/views/Program.vue
+++ src/views/Program.vue
@@ -4,9 +4,12 @@
 
     <v-row class="mb-6" justify="start">
       <v-col md="auto" class="content wrapper">
-        <p>
-          Mer info inom kort.
-        </p>
+        <h2>Program</h2>
+        <p>Mer info om årets program kommer senare i vår.</p>
+        <br />
+        <h2>Tidigare års program</h2>
+        <h3>Program från 2019</h3>
+        <img src="../assets/Tidigare-schema.png" />
       </v-col>
     </v-row>
   </div>
