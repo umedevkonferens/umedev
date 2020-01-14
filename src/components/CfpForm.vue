c9e273ce68db26299c51a76c7956110062384d42
Andrea Berglund <andreaberglund@outlook.com>
2020-01-14 10:05:24 +0100

    Updated cfp form to swedish. Added missing fields from form.

--- src/components/CfpForm.vue
+++ src/components/CfpForm.vue
@@ -4,33 +4,95 @@
       <v-row align="start">
         <v-col>
           <v-form ref="form" v-model="valid" lazy-validation>
-            <v-text-field
-              v-model="proposal.title"
-              :counter="100"
-              :rules="titleRules"
-              label="Titel"
-              required
-            ></v-text-field>
-            <v-textarea
-              v-model="proposal.description"
-              :counter="1000"
-              :rules="descriptionRules"
-              label="Beskrivning"
-              required
-            ></v-textarea>
-            <v-text-field
-              v-model="proposal.name"
-              :counter="30"
-              :rules="nameRules"
-              label="Namn"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="proposal.email"
-              :rules="emailRules"
-              label="E-post"
-              required
-            ></v-text-field>
+            <fieldset>
+              <legend class="bold">Information om bidraget</legend>
+              <v-text-field
+                v-model="proposal.title"
+                :counter="100"
+                :rules="titleRules"
+                label="Titel på bidrag *"
+                required
+              ></v-text-field>
+
+              <v-textarea
+                v-model="proposal.description"
+                :counter="1000"
+                :rules="descriptionRules"
+                label="Beskrivning av bidrag *"
+                required
+              ></v-textarea>
+
+              <fieldset>
+                <legend>Typ av pass *</legend>
+                <v-radio-group
+                  v-model="proposal.typeOfTalk"
+                  :rules="typeOfTalkRules"
+                  required
+                >
+                  <v-radio label="45 min" value="45"></v-radio>
+                  <v-radio label="15 min" value="15"></v-radio>
+                </v-radio-group>
+              </fieldset>
+
+              <v-text-field
+                v-model="proposal.target"
+                label="Målgrupp (vem eller vilka tror du uppskattar ditt pass mest?)"
+              ></v-text-field>
+
+              <fieldset>
+                <legend>Utrustning</legend>
+                <v-row justify="space-around">
+                  <v-checkbox
+                    class="mx-2"
+                    v-model="proposal.equipment.internet"
+                    label="Internet"
+                  ></v-checkbox>
+                  <v-checkbox
+                    class="mx-2"
+                    v-model="proposal.equipment.projector"
+                    label="Projektor"
+                  ></v-checkbox>
+                  <v-checkbox
+                    class="mx-2"
+                    v-model="proposal.equipment.whiteboard"
+                    label="Whiteboard"
+                  ></v-checkbox>
+                  <v-checkbox
+                    class="mx-2"
+                    v-model="proposal.equipment.flipboard"
+                    label="Blädderblock"
+                  ></v-checkbox>
+                </v-row>
+              </fieldset>
+            </fieldset>
+
+            <fieldset>
+              <legend class="bold">Information om talaren</legend>
+              <v-text-field
+                v-model="proposal.name"
+                :counter="30"
+                :rules="nameRules"
+                label="Talarens namn *"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="proposal.descriptionOfSpeaker"
+                :rules="descriptionOfSpeakerRules"
+                label="Beskrivning av dig *"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="proposal.email"
+                :rules="emailRules"
+                label="Email adress *"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="proposal.otherInfo"
+                label="Övrig information"
+              ></v-text-field>
+            </fieldset>
+
             <v-btn
               :disabled="!valid"
               color="success"
@@ -56,30 +118,47 @@ export default Vue.extend({
     snackbar: false,
     proposal: {
       name: '',
+      descriptionOfSpeaker: '',
       title: '',
       email: '',
+      target: '',
       description: '',
+      typeOfTalk: '',
+      otherInfo: '',
+      equipment: {
+        internet: false,
+        projector: false,
+        whiteboard: false,
+        flipboard: false,
+      },
       approved: false,
     },
     nameRules: [
-      (v: string) => !!v || 'Name is required',
+      (v: string) => !!v || 'Namn är obligatoriskt',
       (v: string) =>
-        (v && v.length <= 30) || 'Name must be less than 30 characters',
+        (v && v.length <= 30) || 'Namn måste vara mindre än 30 bokstäver',
     ],
     emailRules: [
-      (v: string) => !!v || 'E-mail is required',
-      (v: string) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      (v: string) => !!v || 'Email adress är obligatoriskt',
+      (v: string) => /.+@.+\..+/.test(v) || 'Email måste vara i rätt format',
     ],
     titleRules: [
-      (v: string) => !!v || 'Field is required',
+      (v: string) => !!v || 'Titel är obligatoriskt',
       (v: string) =>
-        (v && v.length <= 100) || 'Field must be less than 100 characters',
+        (v && v.length <= 100) || 'Titeln måste vara mindre än 100 bokstäver',
     ],
     descriptionRules: [
-      (v: string) => !!v || 'Description is required',
+      (v: string) => !!v || 'Beskrivning är obligatoriskt',
       (v: string) =>
         (v && v.length <= 1000) ||
-        'Description must be less than 1000 characters',
+        'Beskrivningen måste vara mindre än 1000 bokstäver',
+    ],
+    typeOfTalkRules: [(v: string) => !!v || 'Typ av pass är obligatoriskt'],
+    descriptionOfSpeakerRules: [
+      (v: string) => !!v || 'Beskrivning av dig är obligatoriskt',
+      (v: string) =>
+        (v && v.length <= 1000) ||
+        'Beskrivningen måste vara mindre än 1000 bokstäver',
     ],
   }),
   computed: {
@@ -92,6 +171,8 @@ export default Vue.extend({
   }, // Use it like so: this.form.validate()
   methods: {
     submit() {
+      console.log(this.proposal);
+      return;
       if (this.form.validate()) {
         this.snackbar = true;
         proposalsDb.push(this.proposal);
@@ -107,3 +188,12 @@ export default Vue.extend({
   },
 });
 </script>
+<style>
+fieldset {
+  border: 0 none;
+}
+legend {
+  padding-top: 10px;
+  color: rgba(0, 0, 0, 0.6) !important;
+}
+</style>
