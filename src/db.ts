6908482f3769ab61e216c7f476b75634a9c7ff77
Jonas Andersson <jonas.andersson@algoryx.se>
2022-03-05 19:10:15 +0100

    Add firebase, vuefire and fix errors

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
 
 // Get a Firestore instance
 export const db = firebase
